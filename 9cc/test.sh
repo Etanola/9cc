0944e32e69e9a6857eed3f3b95405a3a6ff54884
Etanola <etano2828@gmail.com>
2023-09-16 04:14:00 +0900

    feat: mul, div, single plus/minus, by node tree

--- 9cc/test.sh
+++ 9cc/test.sh
@@ -21,5 +21,10 @@ assert 42 42
 assert 21 "5+20-4"
 assert 100 "13+29+90-32"
 assert 41 "12 + 34 - 5"
+assert 47 "5+6*7"
+assert 15 "5*(9-6)"
+assert 4 "(3+5)/2"
+assert 4 "(3+6)/2"
+assert 10 "-10+20"
 
 echo ok
